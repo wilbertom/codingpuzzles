54e49c430294921a63ede8b69c8afb4fa055282e
Wilberto Morales <wilbertomorales777@gmail.com>
2021-04-04 20:54:25 -0500

    Move reusable code

---
+++ codewars/python/narcissistic_number_test.py
@@ -0,0 +1,8 @@
+from narcissistic_number import narcissistic
+
+
+def test_narcissistic():
+    assert narcissistic(7) is True
+    assert narcissistic(371) is True
+    assert narcissistic(122) is False
+    assert narcissistic(4887) is False
