5692138a9b0080182477910d94787de6f55de2cf
Wilberto Morales <wilbertomorales777@gmail.com>
2021-04-05 22:37:41 -0500

    Improve sort binary tree by levels

--- solutionspy/sort_binary_tree_by_levels.py
+++ solutionspy/sort_binary_tree_by_levels.py
@@ -1,60 +1,28 @@
-def tree_by_levels(node):
-    node = _create_new_tree(0, node)
-    levels = _max_level(node)
-    values = []
-    level = 0
+from collections import defaultdict
 
-    while level <= levels:
-        _add_values_of_level(level, values, node)
-        level += 1
+
+def tree_by_levels(node):
+    values_by_level = _values_by_level(node)
+    values = []
+
+    for values_in_level in values_by_level.values():
+        values.extend(values_in_level)
 
     return values
 
 
-def _add_values_of_level(level, values, node):
+def _values_by_level(node):
+    values = defaultdict(list)
+    _values_by_level_agg(values, 0, node)
+
+    return values
+
+
+def _values_by_level_agg(values, level, node):
     if node is None:
         return
 
-    if node.level == level:
-        values.append(node.value)
+    values[level].append(node.value)
 
-    _add_values_of_level(level, values, node.left)
-    _add_values_of_level(level, values, node.right)
-
-
-def _tree_by_levels(level, values, node):
-    if node is None:
-        return
-
-    if node.level == level:
-        values.append(node.value)
-
-    _tree_by_levels(level, values, node.left)
-    _tree_by_levels(level, values, node.right)
-
-
-def _max_level(node):
-    if node is None:
-        return 0
-
-    return max([_max_level(node.left), _max_level(node.right), node.level])
-
-
-def _create_new_tree(level, node):
-    if node is None:
-        return None
-
-    return _NodeWithLevel(
-        level,
-        _create_new_tree(level + 1, node.left),
-        _create_new_tree(level + 1, node.right),
-        node.value,
-    )
-
-
-class _NodeWithLevel:
-    def __init__(self, level, left, right, value):
-        self.level = level
-        self.left = left
-        self.right = right
-        self.value = value
+    _values_by_level_agg(values, level + 1, node.left)
+    _values_by_level_agg(values, level + 1, node.right)
--- solutionspy/sort_binary_tree_by_levels_test.md
+++ solutionspy/sort_binary_tree_by_levels_test.md
@@ -2,3 +2,10 @@
 
 https://www.codewars.com/kata/52bef5e3588c56132c0003bc/train/python
 
+## AAR
+
+### What went well
+
+### What went bad
+
+### Lessons Learned / Action Items
